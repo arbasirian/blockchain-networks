b9d7e740f097af5634a80c84bf6c43a4e88bfc61
Armin <armin@metatude.org>
2022-05-08 15:06:13 +0430

    Update folders structures

---
+++ src/components/connection-status/connection-status.stories.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import ConnectionStatus from "./connection-status";
+
+export default {
+  title: "Components/ConnectionStatus",
+  component: ConnectionStatus,
+} as ComponentMeta<typeof ConnectionStatus>;
+
+const Template: ComponentStory<typeof ConnectionStatus> = (args) => (
+  <ConnectionStatus {...args} />
+);
+
+export const Active = Template.bind({});
+export const Deactive = Template.bind({});
---
+++ src/components/connection-status/connection-status.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const ConnectionStatus: FC = () => {
+  return <div>ConnectionStatus</div>;
+};
+
+export default ConnectionStatus;
