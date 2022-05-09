19189a6c8dc26c763742d4495cf71dc12de67c4a
Armin <armin@metatude.org>
2022-05-09 14:48:40 +0430

    Fix path issue in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,28 @@
 const path = require("path");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
   ],
-  framework: '@storybook/react',
+  framework: "@storybook/react",
   core: {
-    builder: 'webpack5',
+    builder: "webpack5",
   },
   typescript: { reactDocgen: false },
   webpackFinal: async (config) => {
+    config.resolve.plugins = [new TsconfigPathsPlugin()];
+
     // add SCSS support for CSS Modules
     config.module.rules.push({
       test: /\.scss$/,
-      use: ['style-loader', 'css-loader?modules&importLoaders', 'sass-loader'],
-      include: path.resolve(__dirname, '../'),
+      use: ["style-loader", "css-loader?modules&importLoaders", "sass-loader"],
+      include: path.resolve(__dirname, "../"),
     });
 
     return config;
-}
+  },
 };
